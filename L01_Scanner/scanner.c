4131074852f9236119d8521e4f34be97580391cf
Gia-Huy Do Hust <huydogiaccac@gmail.com>
2024-11-14 09:08:27 +0700

    Update scanner.c

--- L01_Scanner/scanner.c
+++ L01_Scanner/scanner.c
@@ -542,22 +542,6 @@ int scan(char * fileName) {
     return IO_SUCCESS;
 }
 
-/******************************************************************/
-
-/*int main(int argc, char *argv[]) {
-  if (argc <= 1) {
-    printf("scanner: no input file.\n");
-    return -1;
-  }
-
-  if (scan(argv[1]) == IO_ERROR) {
-    printf("Can\'t read input file!\n");
-    return -1;
-  }
-
-  return 0;
-}*/
-
 char * itoa(int num, char * buffer, int base) {
     int current = 0;
     if (num == 0) {
